e2a978d2a3916de882a970c0760da3a8a7995526
pavithramuddu93 <pavithramuddu93>
2022-09-07 10:11:45 +0530

    Employee Wage UC2: Wage calculate

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -20,17 +20,22 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(2); 
+		int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
 
-		if (attendance == 1) {
-			System.out.println("Employee Present..");
-			
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int wagePerDay = 0;
+
+		if (attendance == 2) {
+			System.out.println("Employee Full Day Present..");
+			wagePerDay = wagePerHour * fullDayHour;
 		}
-		 else {
+		 else
 			System.out.println("Employee Absent");
 
-		}
+		System.out.println("Employee Wage Per Day :-" + wagePerDay);
+	}
 
 
 	}
-}
+
