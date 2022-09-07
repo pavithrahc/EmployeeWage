d84bec5834278f19214dd7156c76446106111cc6
pavithramuddu93 <pavithramuddu93>
2022-09-07 09:25:02 +0530

    UC1 Added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -11,18 +11,26 @@ public class EmployeeWage {
 
 	/**
 	 * @param args
+	 * 
+	 * UC1
+	 * EmployeeWage Present or Not
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
-	      
-		Random random= new Random();
-		int attendance=random.nextInt(2);
-		
-		if(attendance == 1)
-			System.out.println("Employee Present..");
-		else
-			System.out.println("Employee Absent..");
-	}
 
+		Random random = new Random();
+		int attendance = random.nextInt(2); 
+
+		if (attendance == 1) {
+			System.out.println("Employee Present..");
+			
+		}
+		 else {
+			System.out.println("Employee Absent");
+
+		}
+
+
+	}
 }
