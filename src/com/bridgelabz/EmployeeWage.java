f1b0de060b87b816631e8faae19334dcfd01f424
pavithramuddu93 <pavithramuddu93>
2022-09-07 10:22:11 +0530

    Employee UC4 : Wage Calculate

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -20,23 +20,38 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
 
 		int wagePerHour = 20;
 		int fullDayHour = 8;
 		int partTimeHour = 4;
 		int wagePerDay = 0;
+		int monthlySalary = 0;
 
-		if (attendance == 2) {
-			System.out.println("Employee Full Day Present....");
-			wagePerDay = wagePerHour * fullDayHour;
-		} else if (attendance == 1) {
-			System.out.println("Employee Part Time Present");
-			wagePerDay = wagePerHour * partTimeHour;
-		} else
-			System.out.println("Employee Absent");
+		for (int day = 1; day <= 20; day++) {
 
-		System.out.println("Employee Wage Per Day :-" + wagePerDay);
+			int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
+			System.out.println("randomNumber"+attendance);
+			wagePerDay =0;
+			switch (attendance) {
+
+			case 0:
+				System.out.println("Employee Absent");
+				break;
+
+			case 1:
+				System.out.println("Employee Part Time Present");
+				wagePerDay = partTimeHour * wagePerHour;
+				break;
+			default:
+				System.out.println("Employee Full Day Present..");
+				wagePerDay = wagePerHour * fullDayHour;
+				break;
+			}
+
+			monthlySalary = monthlySalary + wagePerDay;
+			System.out.println(day + " : " + monthlySalary);
+		}
 	}
 
 }
+	
